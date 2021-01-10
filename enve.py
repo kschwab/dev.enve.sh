c3c9c33002540c5f80d04832f00878dd4524e5f7
Kyle Schwab <schwab.k7@gmail.com>
2021-01-09 18:42:09 -0700

    Checkpoint.

--- enve.py
+++ enve.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
-# TODO: Add option for enve auto switching in jsonnet config - disabled, prompt, enabled
-# TODO: We can skip doing work if already in the shell and the command is not a flatpak app... just simply pass it
-# through or eval it... (ie, this is the runtime enve switching)
-
 # TODO: Add jsonnet option for specifying build spec for extension (for install fail)
 # TODO: Add sandbox option
+# TODO: Add jsonnet sandbox banner option
 # TODO: Look at switching extension/variable configs into object instead of list for explicit overriding (or, add an
 # option that says to use the last extension specified)
 # TODO: Clean up help
@@ -156,7 +153,7 @@ def add_variables(enve_vars: dict, variables: list, extension_alias: str ='', ex
             else:
                 enve_vars[variable_name] = value
 
-def load_enve_config(enve_options: dict) -> [int, dict]:
+def load_enve_config(enve_options: dict) -> dict:
     '''Add doc...'''
 
     # Initialize the ENVE variables dictionary
@@ -164,11 +161,15 @@ def load_enve_config(enve_options: dict) -> [int, dict]:
     # Get the logger instance
     logger = logging.getLogger(__name__)
 
-    enve_config = enve_options['use-config']
+    # Update use-config to defaults if they exist
+    if enve_options['use-config'].lower() == 'base':
+        enve_options['use-config'] = ENVE_BASE_CONFIG_PATH
+    elif not enve_options['use-config']:
+        enve_options['use-config'] = os.environ.get('ENVE_CONFIG', '')
 
     # If the enve_config file is not specified via the command line or environment variable, check to see if it
     # exists in git root directory (if we're in a git repo).
-    if not enve_config:
+    if not enve_options['use-config']:
 
         # Get the git root directory, where a return code of zero means we're not in a git repo.
         completed_output = subprocess.run(['git', 'rev-parse', '--show-toplevel'], capture_output=True, text=True)
@@ -177,24 +178,33 @@ def load_enve_config(enve_options: dict) -> [int, dict]:
             # Check to see if the enve_config.jsonnet file exists in the git root directory.
             git_root_path_enve_config = os.path.join(completed_output.stdout.strip(), 'enve.jsonnet')
             if os.path.exists(git_root_path_enve_config):
-                enve_config = git_root_path_enve_config
+                enve_options['use-config'] = git_root_path_enve_config
 
     # If no enve_config file was specified, prompt user on how to proceed
-    if not enve_config:
+    if not enve_options['use-config']:
         if click.confirm('Unable to locate ENVE config. Would you like you use the base environment?', default=True):
-            enve_config = ENVE_BASE_CONFIG_PATH
+            enve_options['use-config'] = ENVE_BASE_CONFIG_PATH
         else:
             logger.error('Unable to locate ENVE config.')
-            return 1, enve_vars
+            exit(1)
+
+    # If the ENVE config path does not exist, exit with error
+    if not os.path.exists(enve_options['use-config']):
+        logger.error('ENVE config path does not exist: %s', enve_options['use-config'])
+        exit(1)
 
     # Jsonnet will validate the content for us and assert if anything is invalid.
     try:
-        enve_json = json.loads(_jsonnet.evaluate_file(enve_config, import_callback=import_callback))['Enve']
+        enve_json = json.loads(_jsonnet.evaluate_file(enve_options['use-config'], import_callback=import_callback))['Enve']
     except Exception as err:
-        logger.exception('Failed to load ENVE config "%s".', enve_config)
-        return 1, enve_vars
+        logger.exception('Failed to load ENVE config "%s".', enve_options['use-config'])
+        exit(1)
 
+    # Add the ENVE global variables
     add_variables(enve_vars, enve_json['variables'])
+
+    # Add the ENVE prompt variable
+    add_enve_prompt_variable(enve_vars, enve_options)
 
     # Ensure all the specified flatpak extensions are installed with the right commit versions if specified.
     for flatpak_extension in reversed(enve_json['extensions']):
@@ -205,30 +215,27 @@ def load_enve_config(enve_options: dict) -> [int, dict]:
         # Verify the extension is installed, and attempt to install if not found
         if not extension_verify_installed(flatpak_extension):
             logger.error('ENVE load failed.')
-            return 1, enve_vars
+            exit(1)
 
         # Verify the extension commit matches the specified, and attempt to update if SHAs mismatch
         if not extension_verify_commit(flatpak_extension):
             logger.error('ENVE load failed.')
-            return 1, enve_vars
+            exit(1)
 
         # Add the extension load directory paths to the load directories dictionary
         add_variables(enve_vars, flatpak_extension['variables'], flatpak_extension['id_alias'],
                       flatpak_extension['path'])
 
-    add_enve_prompt_variable(enve_vars, enve_options)
-
     logger.debug('ENVE Variables:\n%s', textwrap.indent(pprint.pformat(enve_vars), '  '))
 
-    return 0, enve_vars
+    return enve_vars
 
-def run_cmd(cmd: list, enve_vars: dict, enve_options: dict) -> int:
+def load_cmd_metadata(cmd: list) -> configparser.ConfigParser:
     '''Add doc...'''
 
-    # Get the logger instance
-    logger = logging.getLogger(__name__)
+    cmd_metadata = configparser.ConfigParser()
 
-    # First check to see if the command is a flatpak app
+    # Metadata only exists if the command is a flatpak app
     if re.match('\w+\.\w+\.\w+', cmd[0]):
         # Attempt to get the flatpak metadata information
         completed_output = subprocess.run(['flatpak-spawn', '--host', 'flatpak', 'info', '--show-metadata', cmd[0]],
@@ -237,50 +244,97 @@ def run_cmd(cmd: list, enve_vars: dict, enve_options: dict) -> int:
         # If we're successful with getting the flatpak metadata information, then proceed with running the flatpak app
         # using flatpak-spawn.
         if completed_output.returncode == 0:
-
             # Get the internal command ran by the flatpak app
-            config = configparser.ConfigParser()
-            config.read_string(completed_output.stdout)
-            cmd.insert(1, config['Application']['command'])
-
-            for option in enve_options:
-                cmd += ['--ENVE', str(option), str(enve_options[option])]
-
-            flatpak_spawn_cmd = ['flatpak-spawn', '--host', '--watch-bus', 'flatpak', 'run',
-                                 '--command=%s' % ENVE_PY_PATH,
-                                 '--runtime=%s' % config['Application']['sdk'],
-                                 '--filesystem=host',
-                                 '--socket=session-bus',
-                                 '--allow=devel',
-                                 '--allow=multiarch',
-                                 '--share=network',
-                                 '--device=all',
-                                 '--env=TERM=%s' % os.environ['TERM']]
-
-            logger.debug('Spawn Command:\n%s', textwrap.indent(pprint.pformat(flatpak_spawn_cmd + cmd), '  '))
-
-            # Run the command to completion
-            return subprocess.run(flatpak_spawn_cmd + cmd).returncode
+            cmd_metadata.read_string(completed_output.stdout)
         else:
-            # Log a warning about using suspected flatpak command as regular system command
+            # Log a warning about using suspected flatpak command as regular system command and prompt user on how to
+            # proceed
             logger.warning('Command "%s" suspected as flatpak app but not found.', cmd[0])
-            logger.warning('Treating "%s" as regular system command.', cmd[0])
-            # TODO: PROMPT HERE??
+            if not click.confirm('Treat "%s" as regular system command?' % cmd[0]):
+                logger.error('Invalid command "%s" received.' % cmd[0])
+                exit(1)
 
-    # Export the ENVE variables into the current environment
-    for enve_var in enve_vars:
-        os.environ[enve_var] = enve_vars[enve_var]
+    return cmd_metadata
+
+def run_cmd(cmd: list, enve_options: dict) -> None:
+    '''Add doc...'''
+
+    # Get the logger instance
+    logger = logging.getLogger(__name__)
+
+    # Load the command meta data if the command is a flatpak app. If the command is not a flatpak app,
+    # cmd_metadata.sections() will be [], indicating nothing was loaded or found for the supplied command.
+    cmd_metadata = load_cmd_metadata(cmd)
+
+    if cmd_metadata.sections():
+        # Grab the actual flatpak app command and use it as the command to run enve.py in the spawned environment
+        cmd.insert(1, cmd_metadata['Application']['command'])
+
+        # Pass the ENVE options through to the spawned command
+        for option in enve_options:
+            cmd += ['--ENVE', str(option), str(enve_options[option])]
+
+        flatpak_spawn_cmd = ['flatpak-spawn', '--host', '--watch-bus', 'flatpak', 'run',
+                             '--command=%s' % ENVE_PY_PATH,
+                             '--runtime=%s' % cmd_metadata['Application']['sdk'],
+                             '--filesystem=host',
+                             '--socket=session-bus',
+                             '--allow=devel',
+                             '--allow=multiarch',
+                             '--share=network',
+                             '--device=all',
+                             '--env=TERM=%s' % os.environ['TERM']]
+
+        logger.debug('Spawn Command:\n%s', textwrap.indent(pprint.pformat(flatpak_spawn_cmd + cmd), '  '))
+
+        # Run the command to completion
+        exit(subprocess.run(flatpak_spawn_cmd + cmd).returncode)
+
+    elif enve_options['use-config'] != '' and 'ENVE_ID' in os.environ:
+        # If we're not running a flatpak app, the ENVE use-config option has been specified, and we're currently in an
+        # ENVE shell, then pass the command through to flatpak-spawn to ensure correct isolation of the requested
+        # environment. We could do more complicated diffing between the current ENVE environment and the requested,
+        # however this would require a complex diff for what should be a corner case.
+
+        # Pass the ENVE options through to the spawned command
+        for option in enve_options:
+            cmd += ['--ENVE', str(option), str(enve_options[option])]
+
+        # Note we pass the TERM environment variable here to ensure if colors are supported they show up in the new
+        # shell
+        flatpak_spawn_cmd = ['flatpak-spawn', '--env=TERM=%s' % os.environ['TERM'], ENVE_PY_PATH]
+
+        logger.debug('Spawn Command:\n%s', textwrap.indent(pprint.pformat(flatpak_spawn_cmd + cmd), '  '))
+
+        # Run the command to completion
+        exit(subprocess.run(flatpak_spawn_cmd + cmd).returncode)
+
+    elif 'ENVE_ID' not in os.environ:
+
+        # Parse the ENVE config
+        enve_vars = load_enve_config(enve_options)
+
+        # Ensure that all enve_vars are prefixed with 'ENVE_'
+        bad_enve_vars = [enve_var for enve_var in enve_vars if 'ENVE_' != enve_var[:len('ENVE_')]]
+        if bad_enve_vars:
+            for bad_enve_var in bad_enve_vars:
+                logger.error('ENVE variable not prefixed with "ENVE_": %s', bad_enve_var)
+            exit(1)
+
+        # Export the ENVE variables into the current environment
+        for enve_var in enve_vars:
+            os.environ[enve_var] = enve_vars[enve_var]
 
     if (not enve_options['use-debug-shell']) or click.confirm('Debug shell enabled. Run command "%s"?' % ' '.join(cmd)):
 
         # Run the command to completion
         logger.debug('Run Command:\n%s', textwrap.indent(pprint.pformat(cmd), '  '))
-        errno = subprocess.run([ENVE_RUN_CMD_PATH] + cmd).returncode
+        errno = pty.spawn([ENVE_RUN_CMD_PATH] + cmd)
 
     if enve_options['use-debug-shell']:
-        errno = pty.spawn(ENVE_RUN_CMD_PATH)
+        errno = pty.spawn([ENVE_RUN_CMD_PATH, 'sh'])
 
-    return errno
+    exit(errno)
 
 def enve_options_default() -> dict:
     '''Add doc...'''
@@ -290,7 +344,7 @@ def enve_options_default() -> dict:
 ENVE_OPTION_PARAM = collections.namedtuple('ENVE_OPTION_PARAM', ['default', 'click_type', 'help'])
 ENVE_OPTIONS = {
     'use-config': ENVE_OPTION_PARAM(
-        'base', click.STRING,
+        '', click.STRING,
         '''Path to the enve configuration file. If not specified, the ENVE_CONFIG environment variable will first be
         checked, followed by the "enve.jsonnet" file in the git root directory.
         ... If set, will use the ENVE base config for setting up the environment.'''
@@ -322,59 +376,42 @@ ENVE_OPTIONS = {
 
 @click.command(context_settings={"ignore_unknown_options": True})
 @click.option('--ENVE', type=(click.Choice(ENVE_OPTIONS), str), multiple=True)
-@click.argument('shell_cmd', nargs=-1)
+@click.argument('cmd', nargs=-1)
 @click.pass_context
-def cli(ctx, shell_cmd: tuple, enve: tuple) -> None:
+def cli(ctx, cmd: tuple, enve: tuple) -> None:
     '''Add doc...'''
 
+    # Set the logging default level and format
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+    # Get the logger instance
+    logger = logging.getLogger(__name__)
+    # Set the default log level
+    logger.setLevel(logging.WARNING)
+
+    # Parse the ENVE options
     enve_options = enve_options_default()
     for opt, value in enve:
         try:
             enve_options[opt] = ENVE_OPTIONS[opt].click_type.convert(value, opt, ctx)
         except click.exceptions.ClickException as e:
-            print('Error: --ENVE', opt, 'option', e)
+            logger.error('--ENVE %s option %s', opt, e)
             exit(e.exit_code)
 
-    if enve_options['use-config'].lower() == 'base':
-        enve_options['use-config'] = ENVE_BASE_CONFIG_PATH
-    elif not enve_options['use-config'] and 'ENVE_CONFIG' in os.environ:
-        enve_options['use-config'] = os.environ['ENVE_CONFIG']
-
-    if enve_options['use-config']:
-        try:
-            click.Path(exists=True).convert(enve_options['use-config'], 'use-config', ctx)
-        except click.exceptions.ClickException as e:
-            print('Error: --ENVE', opt, 'option', e)
-            exit(e.exit_code)
-
-    # Set the logging default level and format
-    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
-
-    # Get the logger instance
-    logger = logging.getLogger(__name__)
-    # Set the default log level
-    logger.setLevel(logging.WARNING)
     # Update the logger verbosity level if specified
     if 'debug' == enve_options['use-verbose']:
         logger.setLevel(logging.DEBUG)
-        logger.info('Debug log level set.')
     elif 'info' == enve_options['use-verbose']:
         logger.setLevel(logging.INFO)
-        logger.info('Info log level set.')
 
-    # Parse the ENVE config
-    errno, enve_vars = load_enve_config(enve_options)
+    # Convert the cmd tuple into a list
+    cmd = list(cmd)
 
     # If no command is specified, start the shell by default
-    if not shell_cmd:
-        shell_cmd = ('sh',)
+    if not cmd:
+        cmd = ['sh']
 
     # Run the requested shell cmd using ENVE only if the ENVE load was successful
-    if errno == 0:
-        errno = run_cmd(list(shell_cmd), enve_vars, enve_options)
-
-    # Exit with error code
-    exit(errno)
+    run_cmd(cmd, enve_options)
 
 if __name__ == '__main__':
     cli(prog_name=os.environ['FLATPAK_ID'])
